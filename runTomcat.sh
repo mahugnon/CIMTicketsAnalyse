a2967dc9d3856d1145c744e724aca5d3f42ab80b
mahugnon <homahugnon@gmail.com>
2021-02-22 16:02:11 +0100

    Update runTomcat.sh
    change jar file path

--- runTomcat.sh
+++ runTomcat.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Ddb2.jcc.charsetDecoderEncoder=3 -jar  ./CIMRestEndPointForTicketDataset-0.0.1-SNAPSHOT.jar  --server.port=9599 
+java -Ddb2.jcc.charsetDecoderEncoder=3 -jar  pharo-local/iceberg/mahugnon/CIMTicketsAnalyse/CIMRestEndPointForTicketDataset-0.0.1-SNAPSHOT.jar  --server.port=9599 
